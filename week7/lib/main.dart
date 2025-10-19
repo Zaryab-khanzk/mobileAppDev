207ef9ea09bcb38c157937dcfa7023ccc7056980
Zaryab-khan <zaryabkhan3047@gmail.com>
2025-10-19 12:44:54 +0500

    flutter playground week 7

--- week7/lib/main.dart
+++ week7/lib/main.dart
@@ -1,24 +1,60 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main(){
-  runApp(const MyApp());
+void main() {
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'Hello World',
-            style: TextStyle(fontSize: 60),
-            
-          ),
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 19, 151, 7)),
         ),
-      )
+        home: MyHomePage(),
+      ),
     );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 97, 205, 120),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: BigCard(pair),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> BigCard(WordPair pair) {
+    return [
+      const Text('A random idea and more: '),
+      Text(pair.asLowerCase),
+      ElevatedButton(
+        onPressed: () {
+          print('button pressed');
+        },
+        child: const Text('next'),
+      ),
+    ];
   }
 }
