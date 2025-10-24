305e4b3a5c52bc6be82be87f7d2955e0dc3453ae
Zaryab-khan <zaryabkhan3047@gmail.com>
2025-10-24 14:32:46 +0500

    some practice

--- practiceapp/lib/main.dart
+++ practiceapp/lib/main.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-    Center(
-      child: Text('Hello world',
-      textDirection: TextDirection.ltr, 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Home'),
       ),
-    ),
-    
-  );
+      body: Center(
+        child: Text(
+          'Hello world',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
 }
